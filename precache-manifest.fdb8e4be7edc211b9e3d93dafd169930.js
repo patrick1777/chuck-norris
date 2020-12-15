0d2bf69766c5b55f2b7619ae77858eb7b39934eb
patrick1777 <patricknjihia201607@gmail.com>
2020-12-15 20:47:22 +0300

    Updates

---
+++ precache-manifest.fdb8e4be7edc211b9e3d93dafd169930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412859c64fae4ec501ef4c50689db24e",
+    "url": "/chuck-norris/public/index.html"
+  },
+  {
+    "revision": "09d61de03938a1372e30",
+    "url": "/chuck-norris/public/static/js/2.f0f70dca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/chuck-norris/public/static/js/2.f0f70dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b28d6db7f4ed614545",
+    "url": "/chuck-norris/public/static/js/main.b511f772.chunk.js"
+  },
+  {
+    "revision": "f98ec75cf1f2ce20b307",
+    "url": "/chuck-norris/public/static/js/runtime-main.948a48f1.js"
+  }
+]);
